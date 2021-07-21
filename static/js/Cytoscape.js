6a71b15faafc701d28cc361c5573fc05f939faa5
Spencer Arnold <spencerlarnold2@gmail.com>
2021-07-21 04:34:17 -0400

    Polished menus and added functionality

--- static/js/Cytoscape.js
+++ static/js/Cytoscape.js
@@ -1,91 +1,131 @@
-var cy = cytoscape({
+var vID;
+var eID;
+function addVertexTop() {
 
-  container: document.getElementById('cy'), // container to render in
+    var eles = cy.add([
+        { group: 'nodes', data: { label: 'n' + vID, id: 'n' + vID++, }, position: { x: 100, y: 100 } }
+    ]);
 
-  elements: [ // list of graph elements to start with
-    // { // node a
-    //   data: { id: 'a' }
-    // },
-    // { // node b
-    //   data: { id: 'b' }
-    // },
-    // { // edge ab
-    //   data: { id: 'ab', source: 'a', target: 'b' }
-    // }
-  ],
+}
 
-  style: [ // the stylesheet for the graph
-    {
-      selector: 'node',
-      style: {
-        'background-color': '#666',
-        'label': 'data(id)'
-      }
-    },
+function addEdgeTop() {
+    var node1, node2;
+    cy.on('tap', 'node', function (evt) {
+        var node = evt.target;
+        if (node1 == null) {
+            node1 = node.id();
+        }
+        else {
+            node2 = node.id();
+        }
+        if (node1 != null && node2 != null) {
+            var eles = cy.add([
+                { group: 'edges', data: { label: '', id: 'e' + eID++, source: node1, target: node2 } }
+            ]);
+            node1 = null;
+            node2 = null;
+            cy.removeListener('tap');
+        }
+    });
+}
 
-    {
-      selector: 'edge',
-      style: {
-        'width': 3,
-        'line-color': '#ccc',
-        'target-arrow-color': '#ccc',
-        'target-arrow-shape': 'triangle',
-        'curve-style': 'bezier'
-      }
+function deleteTop() {
+    cy.on('tap', 'node', function (evt) {
+        var node = evt.target;
+        if (node != null) {
+            cy.remove(node);
+            cy.removeListener('tap');
+        }
+    });
+    cy.on('tap', 'edge', function (evt) {
+        var edge = evt.target;
+        if (edge != null) {
+            cy.remove(edge);
+            cy.removeListener('tap');
+        }
+    });
+}
+
+
+function newDirected() { // creates new cytoscape for directed graphs
+    // cy.destroy();
+    vID = 0;
+    eID = 0;
+    cy = cytoscape({
+
+        container: document.getElementById('cy'), // container to render in
+    
+        elements: [],
+    
+        style: [ // the stylesheet for the graph
+            {
+                selector: 'node',
+                style: {
+                    'background-color': '#666',
+                    'label': 'data(label)'
+                }
+            },
+    
+            {
+                selector: 'edge',
+                style: {
+                    'width': 3,
+                    'line-color': '#ccc',
+                    'target-arrow-color': '#ccc',
+                    'target-arrow-shape': 'triangle', //difference between the two graphs
+                    'curve-style': 'bezier',
+                    'label': 'data(label)'
+                }
+            }
+        ],
+    
+        layout: {
+            name: 'grid',
+            rows: 1
+        }
+    
+    });
+}
+
+function newUndirected() { // creates new cytoscape for undirected graphs
+
+    vID = 0;
+    eID = 0;
+    cy = cytoscape({
+
+    container: document.getElementById('cy'), // container to render in
+
+    elements: [],
+
+    style: [ // the stylesheet for the graph
+        {
+            selector: 'node',
+            style: {
+                'background-color': '#666',
+                'label': 'data(label)',
+            }
+        },
+
+        {
+            selector: 'edge',
+            style: {
+                'width': 3,
+                'line-color': '#ccc',
+                'target-arrow-color': '#ccc',
+                'target-arrow-shape': 'none',
+                'curve-style': 'bezier',
+                'label': 'data(label)'
+            }
+        }
+    ],
+
+    layout: {
+        name: 'grid',
+        rows: 1
     }
-  ],
-
-  layout: {
-    name: 'grid',
-    rows: 1
-  }
 
 });
-var vID =0;
-var eID =0;
-
-function addVertexTop(){
-
-  var eles = cy.add([
-    { group: 'nodes', data: { id: 'n' + vID++ }, position: { x: 100, y: 100 } }
-  ]);
-
 }
 
-function addEdgeTop(){
-  var node1, node2;
-  cy.on('tap', 'node', function(evt){
-    var node = evt.target;
-    if(node1==null){
-      node1 = node.id();
-    }
-    else{
-      node2 = node.id();
-    }
-    if(node1 != null && node2 != null){
-      var eles = cy.add([
-        { group: 'edges', data: { source: node1, target: node2 } }
-      ]);
-      node1 = null;
-      node2 = null;
-      cy.removeListener('tap');
-    }
-  });
-}
 
-function deleteTop(){
-  cy.on('tap', 'node', function(evt){
-    var node = evt.target;
-    if(node!=null){
-      cy.remove(node);
-      cy.removeListener('tap');
-    }
-  });
-  cy.on('tap', 'edge', function(evt){
-    var edge = evt.target;
-    if(edge!=null){
-      cy.remove(edge);
-      cy.removeListener('tap');
-    }
-  });
-}
+
