16b6c146df4a872e589e0fdac27bb9677378f6fa
Spencer Arnold <spencerlarnold2@gmail.com>
2021-11-17 04:21:14 -0500

    Flask app changes

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
+homeRoute="/eggview"
 
-@app.route('/')
+@app.route(homeRoute+'/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/<path:path>')
+def static_file(path):
+    return app.send_static_file(path)
+
+if(__name__ == "__main__"):
+    app.run(host='0.0.0.0')
